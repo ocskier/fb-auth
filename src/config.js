7d95c7c421c6cacc679fb44940594c64e0d3259d
Jon Jackson <36890724+ocskier@users.noreply.github.com>
2020-07-13 01:29:56 -0400

    Delete config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyD_tgpw_aI3elBJ3FQzH5kqi00Qep6jXxM',
-  authDomain: 'projectcodingcamp.firebaseapp.com',
-  databaseURL: 'https://projectcodingcamp.firebaseio.com',
-  projectId: 'projectcodingcamp',
-  storageBucket: 'projectcodingcamp.appspot.com',
-  messagingSenderId: '277978229879',
-  appId: '1:277978229879:web:d36cd80c865f646856aa72',
-};
